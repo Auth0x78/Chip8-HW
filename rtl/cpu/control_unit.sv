2ad176b384ff5712e4bdd04a4b4afd34708ebb9e
Auth0x78 <auth0x78+github@gmail.com>
2026-09-08 07:06:37 +0530

    [Incomplete]: Change to single cycle FDE

--- rtl/cpu/control_unit.sv
+++ rtl/cpu/control_unit.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 `include "control_unit_params.vh"
 
-module control_unit(
+module control_unit (
     // Clock and rst
     input wire clk,
     input wire rst,
@@ -11,38 +11,13 @@ module control_unit(
 
     // Address output to memory
     output wire [15:0] mem_addr,
-    output wire [7:0] out_mem_data
+    output wire [ 7:0] out_mem_data
 );
 
-// Control Unit Internal Registers
-reg [15:0] pc;
-reg [7:0] sp; 
-reg [15:0] instr_reg; // Instruction Cache Register
-reg [1:0] state = CU_STATE_FETCH; // Current state of CPU
+  // Control Unit Internal Registers
+  reg [15:0] pc;
+  reg [ 7:0] sp;
+  reg [15:0] instr_reg;  // Instruction Cache Register
 
-// Combinational Logic for next addr
-wire [15:0] next_pc;
-
-// Assign mem_addr as PC or custom memory address
-assign mem_addr = pc;
-
-// Initial value of state should be FETCH
-initial begin
-    state = CU_STATE_FETCH;
-end
-
-always_comb begin
-    next_pc = pc + 2;
-end
-
-always_ff @(posedge clk or posedge rst) begin
-    if (rst) begin
-        state <= CU_STATE_FETCH;
-        pc <= 16'h0200;
-    end else begin
-        
-        instr_reg <= in_data;
-    end
-end
-    
+  
 endmodule
